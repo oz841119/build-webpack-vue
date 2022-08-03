bf48942b0ad878b24c88f01f28a31bf9229506ca
oz841119 <oz841119@gmail.com>
2022-08-03 23:11:17 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    <div>手動搭建webpack環境使用Vue3</div>
+  </div>
+</template>
+<script>
+export default {
+  setup() {}
+}
+</script>
+
+<style>
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader') // vue loader須額外引入並掛入plugins(*1)
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(), // vue loader須額外引入並掛入plugins(*1)
+    new HtmlWebpackPlugin({template: "./public/index.html"}),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 8080,
+    open: true
+  }
+}
