3a4d9ea1345e26af832ffb0d94c16bccd77974c5
oz841119 <oz841119@gmail.com>
2022-08-04 01:04:15 +0800

    file loader

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <h1>搭建Webpack環境使用Vue3</h1>
+  <img src="../assets/images/頭上裝攝影機.jpg" alt="">
 </template>
 
-<style lang='scss' scoped>
+<style lang='scss'>
   h1 {
     font-size: 12px;
   }
